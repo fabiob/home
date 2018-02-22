542e234190583705c10879d1eb2d65fc8aedbaa4
Fábio D. Batista <fabio@elementarsistemas.com.br>
2018-02-21 22:49:19 -0300

    Desligada inversão do scroll

--- .profile-modules/modules/mouse-sensitivity.sh
+++ .profile-modules/modules/mouse-sensitivity.sh
@@ -2,5 +2,5 @@
 if [[ $XDG_SESSION_TYPE == "x11" ]]; then
   xset m 10 4
   synclient HorizTwoFingerScroll=1 PalmDetect=1 PalmMinWidth=10 PalmMinZ=100 \
-            LockedDrags=1 VertScrollDelta=-79 HorizScrollDelta=-79
+            LockedDrags=1 #VertScrollDelta=-79 HorizScrollDelta=-79
 fi
