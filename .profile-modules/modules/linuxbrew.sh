e1c94fb6d5121f87a37c782fa7120fe6aeba2e99
Fabio Batista <fabio@atelie.dev.br>
2022-11-19 18:31:57 -0300

    Updates homebrew scripts

--- .profile-modules/modules/linuxbrew.sh
+++ .profile-modules/modules/linuxbrew.sh
@@ -1,2 +1,20 @@
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
+if type brew &>/dev/null
+then
+  HOMEBREW_PREFIX="$(brew --prefix)"
+
+  # adds pip-installed executables to the PATH
+  export PATH=${HOMEBREW_PREFIX}/opt/python/libexec/bin:$PATH
+
+  # loads autocompletion scripts
+  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
+  then
+    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+  else
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
+    do
+      [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+    done
+  fi
+fi
