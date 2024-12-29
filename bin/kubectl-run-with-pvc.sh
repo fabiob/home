32426a370fd0ae23a38bb06e0d1167ff8d4d3e71
Fabio Batista <fabio@atelie.dev.br>
2024-12-29 17:28:55 -0300

    Updates kubectl-run-with-pvc to allow explicit namespace

--- bin/kubectl-run-with-pvc.sh
+++ bin/kubectl-run-with-pvc.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-IMAGE="gcr.io/google-containers/ubuntu-slim:0.14"
-COMMAND="/bin/bash"
+IMAGE="alpine"
+COMMAND="/bin/sh"
 SUFFIX=$(date +%s | shasum | base64 | fold -w 10 | head -1 | tr '[:upper:]' '[:lower:]')
+KARGS=""
 
 usage_exit() {
-    echo "Usage: $0 [-c command] [-i image] PVC ..." 1>&2
+    echo "Usage: $0 [-c command] [-i image] [-n namespace] PVC ..." 1>&2
     exit 1
 }
 
-while getopts i:h OPT
+while getopts cin:h OPT
 do
     case $OPT in
         i)  IMAGE=$OPTARG
             ;;
         c)  COMMAND=$OPTARG
+            ;;
+        n)  KARGS="$KARGS -n $OPTARG"
             ;;
         h)  usage_exit
             ;;
@@ -30,22 +33,19 @@ COMMA=""
 
 for i in $@
 do
-  VOL_MOUNTS="${VOL_MOUNTS}${COMMA}{\"name\": \"${i}\",\"mountPath\": \"/pvcs/${i}\"}"
+  VOL_MOUNTS="${VOL_MOUNTS}${COMMA}{\"name\": \"${i}\",\"mountPath\": \"/mnt/${i}\"}"
   VOLS="${VOLS}${COMMA}{\"name\": \"${i}\",\"persistentVolumeClaim\": {\"claimName\": \"${i}\"}}"
   COMMA=","
 done
 
-kubectl run -it --rm --restart=Never --image=${IMAGE} pvc-mounter-${SUFFIX} --overrides "
+set -x
+kubectl run -it --rm --restart=Never $KARGS --image=${IMAGE} pvc-mounter-${SUFFIX} --overrides "
 {
   \"spec\": {
-    \"hostNetwork\": true,
-    \"containers\":[
+    \"containers\": [
       {
-        \"args\": [\"${COMMAND}\"],
-        \"stdin\": true,
-        \"tty\": true,
-        \"name\": \"pvc\",
-        \"image\": \"${IMAGE}\",
+        \"image\": \"${IMAGE}\", \"args\": [\"${COMMAND}\"],
+        \"stdin\": true, \"tty\": true, \"name\": \"pvc\",
         \"volumeMounts\": [
           ${VOL_MOUNTS}
         ]
